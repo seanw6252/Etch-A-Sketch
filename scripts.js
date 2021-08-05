8fc1c198f2c27c36d03ad10875047233709f2601
Sean Wong <s3786110@student.rmit.edu.au>
2021-08-06 07:03:15 +1000

    Finished Etch-A-Sketch project

--- scripts.js
+++ scripts.js
@@ -9,6 +9,57 @@ function makeGrid(rows, columns) {
     container.style.setProperty('--grid-columns', columns);
     for (let i = 0; i < (rows * columns); i++) {
         let gridSquare = document.createElement('div');
+        gridSquare.addEventListener('mouseover', (e) => {
+            if (eraseMode) {
+                e.target.style.backgroundColor = 'white';
+            } else if (rainbowMode) {
+                e.target.style.backgroundColor = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
+            } else {
+                e.target.style.backgroundColor = 'black';
+            }
+        });
         container.appendChild(gridSquare).className = "grid-square";
     }
+
 }
+
+function resetGrid() {
+    let gridRows = getComputedStyle(document.documentElement).getPropertyValue("--grid-rows");
+    makeGrid(gridRows, gridRows);
+}
+
+function resizeGrid() {
+    let newSize = prompt("Enter a new size (Maximum of 100):");
+    if (Number(newSize)) {
+        if (Number(newSize > 0 && newSize < 100)) {
+            makeGrid(newSize, newSize);
+        } else {
+            alert("Please enter a valid number between 0 and 100.")
+        }
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+let rainbowMode = false;
+let eraseMode = false;
+
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener('click', resetGrid);
+
+const resizeButton = document.getElementById("resize-button");
+resizeButton.addEventListener('click', resizeGrid);
+
+const rainbowButton = document.getElementById("rainbow-button");
+rainbowButton.addEventListener('click', () => {
+    rainbowMode = !rainbowMode;
+});
+
+const eraseButton = document.getElementById("erase-button");
+eraseButton.addEventListener('click', () => {
+    eraseMode = !eraseMode;
+});
+
+makeGrid(16, 16)
